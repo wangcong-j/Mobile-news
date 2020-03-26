42f95399d92908f3995be64b31cacadef22f46a0
unknown <you@example.com>
2020-03-26 14:51:35 +0800

    项目起步 vant / rem

--- src/main.js
+++ src/main.js
@@ -2,8 +2,15 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+// 导入rem适配文件
+import 'amfe-flexible'
+
+import Vant from 'vant'
+import 'vant/lib/index.css'
 
 Vue.config.productionTip = false
+
+Vue.use(Vant)
 
 new Vue({
   router,
